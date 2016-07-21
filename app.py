031104777e4afe2272a7b5da675874d7e672b758
Erik Nguyen <namkhanh164@gmail.com>
2016-07-21 20:19:00 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#define the basic route
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+if __name__ == "__main__":
+	app.run()
